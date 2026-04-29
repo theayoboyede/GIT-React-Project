5bf8d5523aef1885038c7405ecf3e4c063291674
Ayoboyede <ayoboyedeadebesin@gmail.com>
2026-04-29 01:25:07 +0100

    Built basic todo app with add/delete

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,50 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { View, Text, StyleSheet, Pressable, TextInput, ScrollView } from 'react-native';
+import { useState } from 'react';
 
 export default function HomeScreen() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  // Add task
+  const addTask = () => {
+    if (task.trim() === '') return;
+
+    setTasks([...tasks, task]);
+    setTask('');
+  };
+
+  // Delete task
+  const deleteTask = (index: number) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Ayoboyede's App</Text>
+      <Text style={styles.title}>My To-Do App</Text>
 
-      <Text style={styles.subtitle}>First React Native project 🚀</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter a task..."
+        value={task}
+        onChangeText={setTask}
+      />
 
-      <Pressable style={styles.button} onPress={() => alert("Button pressed")}>
-        <Text style={styles.buttonText}>Tap Me</Text>
+      <Pressable style={styles.button} onPress={addTask}>
+        <Text style={styles.buttonText}>Add Task</Text>
       </Pressable>
+
+      <ScrollView style={styles.taskContainer}>
+        {tasks.map((item, index) => (
+          <Pressable
+            key={index}
+            onPress={() => deleteTask(index)}
+            style={styles.taskItem}
+          >
+            <Text style={styles.taskText}>• {item}</Text>
+          </Pressable>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -17,30 +52,49 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     padding: 20,
+    backgroundColor: '#fff',
   },
 
   title: {
     fontSize: 26,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-
-  subtitle: {
-    fontSize: 16,
+    fontWeight: 'bold',
     marginBottom: 20,
   },
 
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+
   button: {
-    backgroundColor: "#111",
+    backgroundColor: '#111',
     padding: 15,
-    borderRadius: 10,
+    borderRadius: 8,
+    marginBottom: 20,
   },
 
   buttonText: {
-    color: "#fff",
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  taskContainer: {
+    marginTop: 10,
+  },
+
+  taskItem: {
+    backgroundColor: '#f2f2f2',
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 10,
+  },
+
+  taskText: {
     fontSize: 16,
   },
 });
